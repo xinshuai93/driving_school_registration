53378dc4fc767213c2fda5b0593fd631db22ce85
LonerWJ <1104850148@qq.com>
2023-06-14 20:29:17 +0800

    ioi

--- service/src/main/java/com/drive/driveservice/controller/CoachController.java
+++ service/src/main/java/com/drive/driveservice/controller/CoachController.java
@@ -4,15 +4,19 @@ package com.drive.driveservice.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.drive.commonutils.R;
+import com.drive.driveservice.dto.CoachDTO;
 import com.drive.driveservice.entity.Coach;
+import com.drive.driveservice.entity.Student;
 import com.drive.driveservice.entity.vo.CoachQuery;
 import com.drive.driveservice.service.CoachService;
+import com.drive.driveservice.service.StudentService;
 import io.swagger.annotations.ApiOperation;
 import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -30,6 +34,9 @@ public class CoachController {
 
     @Autowired
     private CoachService coachService;
+
+    @Autowired
+    private StudentService studentService;
 
     @ApiOperation("新增教练")
     @PostMapping("addCoach")
@@ -74,5 +81,34 @@ public class CoachController {
         return R.ok().data("total",total).data("records",records);
     }
 
+    @ApiOperation("查询自己学员")
+    @PostMapping("getSelfStu/{page}/{limit}")
+    public R getSelfStu(@PathVariable Long page,
+                        @PathVariable Long limit,
+                        @RequestBody(required = false) CoachDTO dto){
+        Page<Student> studentPage = new Page<>(page,limit);
+        QueryWrapper<Student> wrapper = new QueryWrapper<>();
+        wrapper.eq("coach_id",dto.getCoachId());
+        String name = dto.getName();
+        String phone = dto.getPhone();
+        Date begin = dto.getBegin();
+        Date end = dto.getEnd();
+        if (!StringUtils.isEmpty(name)){
+            wrapper.like("name",name);
+        }
+        if (!StringUtils.isEmpty(phone)){
+            wrapper.like("phone",phone);
+        }
+        if (!StringUtils.isEmpty(begin)){
+            wrapper.ge("registe_time",begin);
+        }
+        if (!StringUtils.isEmpty(end)){
+            wrapper.le("resigte_time",end);
+        }
+        studentService.page(studentPage,wrapper);
+        Long total = studentPage.getTotal();
+        List<Student> records = studentPage.getRecords();
+        return R.ok().data("total",total).data("records",records);
+    }
 }
 
