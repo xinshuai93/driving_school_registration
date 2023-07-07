471355d390c36f484416eb0473c5082ffb0a32fe
xinshuai <xinshuai93@gmail.com>
2023-07-07 15:26:25 +0800

    7.7 预约练车
    Signed-off-by: xinshuai <xinshuai93@gmail.com>

---
+++ service/src/main/java/com/drive/driveservice/service/impl/ExerciseServiceImpl.java
@@ -0,0 +1,20 @@
+package com.drive.driveservice.service.impl;
+
+import com.drive.driveservice.entity.Exercise;
+import com.drive.driveservice.mapper.ExerciseMapper;
+import com.drive.driveservice.service.ExerciseService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 预约练车表 服务实现类
+ * </p>
+ *
+ * @author lx
+ * @since 2023-07-07
+ */
+@Service
+public class ExerciseServiceImpl extends ServiceImpl<ExerciseMapper, Exercise> implements ExerciseService {
+
+}
